d98b3ef9b35dc72ce5f01b94c713dfd1eb98a1b0
proto-pic <martin.usher@proto-pic.co.uk>
2021-10-08 06:25:18 +0100

    Update to file
    This updated and fixed code has been very kindly provided by https://github.com/pierreyvesbaloche
    
    
    https://hotsolder.co.uk

--- Simon_PCB.py
+++ Simon_PCB.py
@@ -12,143 +12,140 @@ plus = Image("00000:"
              "00000")
 
 def clear():
-    np[0] = (0,0,0)
-    np[1] = (0,0,0)
-    np[2] = (0,0,0)
-    np[3] = (0,0,0)
+    np[0] = (0, 0, 0)
+    np[1] = (0, 0, 0)
+    np[2] = (0, 0, 0)
+    np[3] = (0, 0, 0)
     np.show()
-    
+
 def doALED():
     clear()
-    np[0] = (128,0,0)
+    np[0] = (128, 0, 0)
     np.show()
-    
+
 def doBLED():
     clear()
-    np[1] = (0,128,0)
+    np[1] = (0, 128, 0)
     np.show()
-    
+
 def doCLED():
     clear()
-    np[3] = (0,0,128)
+    np[3] = (0, 0, 128)
     np.show()
-    
+
 def doDLED():
     clear()
-    np[2] = (128,128,0)
+    np[2] = (128, 128, 0)
     np.show()
-    
+
 def doABeep():
     tune = ["C4:2"]
     music.play(tune)
-    
+
 def doBBeep():
     tune = ["D4:2"]
     music.play(tune)
-    
+
 def doCBeep():
     tune = ["E4:2"]
     music.play(tune)
-    
+
 def doDBeep():
     tune = ["F4:2"]
     music.play(tune)
-    
+
 def doAllOn():
     for pix in range(0 , 4):
-            np[pix] = (64,64,64)
-            np.show()
-            
+        np[pix] = (64, 64, 64)
+        np.show()
+
 def flashReady():
     for flashRDY in range(0 , 4):
-            doAllOn()
-            sleep(50)
-            clear()
+        doAllOn()
+        sleep(50)
+        clear()
 
-simonsString = ["A", "B", "C", "D"]
+simonsS = ["A", "B", "C", "D"]
 
-sequence = random.choice(simonsString) + random.choice(simonsString) + random.choice(simonsString)
-            
+sequence = random.choice(simonsS) + random.choice(simonsS) + random.choice(simonsS)
+
 correct = True
 sleep(1000)
 
 while correct == True:
-  for character in sequence:
-        if character=="A":
+    for character in sequence:
+        if character == "A":
             doALED()
             doABeep()
-        elif character=="B":
+        elif character == "B":
             doBLED()
             doBBeep()
-        elif character=="C":
+        elif character == "C":
             doCLED()
             doCBeep()
-        elif character=="D":
+        elif character == "D":
             doDLED()
             doDBeep()
         sleep(500)
         display.show(plus)
         clear()
         sleep(500)
-   
-  flashReady()
-  maxInputs = len(sequence)
-  capturedInputs = 0
-  while capturedInputs < maxInputs and correct == True:
-     if pin16.read_digital() == 0:
-        doALED()
-        doABeep()
-        #Did the user guess it wrong? 
-        if sequence[capturedInputs] != "A":
-            correct = False
-        sleep(200)    
-        display.show(plus)        
-        capturedInputs += 1
-        
-        
-     if pin1.read_digital() == 0:
-        doBLED()
-        doBBeep()
-       #Did the user guess it wrong? 
-        if sequence[capturedInputs] != "B":
-            correct = False
-        sleep(200)
-        display.show(plus)
-        capturedInputs += 1
-        
-        
-     if pin12.read_digital() == 0:
-        doCLED()
-        doCBeep()
-       #Did the user guess it wrong? 
-        if sequence[capturedInputs] != "C":
-            correct = False
-        sleep(200)
-        display.show(plus)
-        capturedInputs += 1 
-        
-        
-     if pin2.read_digital() == 0:
-        doDLED()
-        doDBeep()
-       #Did the user guess it wrong? 
-        if sequence[capturedInputs] != "D":
-            correct = False
-        sleep(200)
-        display.show(plus)
-        capturedInputs += 1         
-        
-        
-  #Add an extra character to the sequence
-  if correct == True:
-        sequence = sequence + random.choice(simonsString)
+
+    flashReady()
+    maxInputs = len(sequence)
+    capturedInputs = 0
+    while capturedInputs < maxInputs and correct == True:
+        if pin16.read_digital() == 0:
+            doALED()
+            doABeep()
+            #Did the user guess it wrong?
+            if sequence[capturedInputs] != "A":
+                correct = False
+            sleep(200)
+            display.show(plus)
+            capturedInputs += 1
+
+        if pin1.read_digital() == 0:
+            doBLED()
+            doBBeep()
+            # Did the user guess it wrong?
+            if sequence[capturedInputs] != "B":
+                correct = False
+            sleep(200)
+            display.show(plus)
+            capturedInputs += 1
+
+        if pin12.read_digital() == 0:
+            doCLED()
+            doCBeep()
+            # Did the user guess it wrong?
+            if sequence[capturedInputs] != "C":
+                correct = False
+            sleep(200)
+            display.show(plus)
+            capturedInputs += 1
+
+        if pin2.read_digital() == 0:
+            doDLED()
+            doDBeep()
+            # Did the user guess it wrong?
+            if sequence[capturedInputs] != "D":
+                correct = False
+            sleep(200)
+            display.show(plus)
+            capturedInputs += 1
+
+
+    # Add an extra character to the sequence
+    if correct == True:
+        sequence = sequence + random.choice(simonsS)
         display.show(Image.HAPPY)
         sleep(1000)
 
-#Display Game Over  + final score
-if len(sequence)>3:
-    music.play(music.FUNERAL)    
-    display.scroll("Game Over: Score: " + str(len(sequence))) 
+# Display Game Over  + final score
+if len(sequence) > 3:
+    music.play(music.FUNERAL)
+    display.scroll("Game Over: Score: " + str(len(sequence)))
 else:
     music.play(music.FUNERAL)
     display.scroll("Game Over: Score: 0")
